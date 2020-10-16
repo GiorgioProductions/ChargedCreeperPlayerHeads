6af0045ec80dcd36bf6169ed56d4c28247a1f0f0
Giorgio Productions <giorgioproductions@gmail.com>
2020-10-16 19:41:19 +0200

    Import

---
+++ src/es/giorgioproductions/chargedcreeperplayerheads/ExplosionListener.java
@@ -0,0 +1,36 @@
+package es.giorgioproductions.chargedcreeperplayerheads;
+
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+public class ExplosionListener implements Listener {
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onEntityDeath(final EntityDamageByEntityEvent e) {
+        Entity ent = e.getEntity();
+        if (ent instanceof Player) {
+            Player p = (Player) ent;
+            if (e.getDamager() instanceof Creeper) {
+                if(p.getHealth() - e.getDamage() <= 0) {
+                    Creeper creeper = (Creeper)e.getDamager();
+                    if (creeper.isPowered()) {
+                        ItemStack playerhead = new ItemStack(Material.PLAYER_HEAD,1);
+                        SkullMeta playerheadmeta = (SkullMeta) playerhead.getItemMeta();
+                        playerheadmeta.setOwningPlayer((OfflinePlayer) p.getPlayer());
+                        playerhead.setItemMeta(playerheadmeta);
+                        ent.getWorld().dropItemNaturally(ent.getLocation(),playerhead);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/es/giorgioproductions/chargedcreeperplayerheads/Main.java
@@ -0,0 +1,14 @@
+package es.giorgioproductions.chargedcreeperplayerheads;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("§bChargedCreeperPlayerHeads §8was loaded successfully");
+        getServer().getPluginManager().registerEvents(new ExplosionListener(), this);
+    }
+
+}
