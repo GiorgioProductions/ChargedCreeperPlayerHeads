070d0a1e4ef8071f027b0625f06ab42c211d0a1b
Giorgio Productions <giorgioproductions@gmail.com>
2020-10-20 13:53:36 +0200

    update to 1.1

--- src/es/giorgioproductions/chargedcreeperplayerheads/Main.java
+++ src/es/giorgioproductions/chargedcreeperplayerheads/Main.java
@@ -7,7 +7,7 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage("§bChargedCreeperPlayerHeads §8was loaded successfully");
+        Bukkit.getConsoleSender().sendMessage("§bChargedCreeperPlayerHeads v1.1 §8was loaded successfully");
         getServer().getPluginManager().registerEvents(new ExplosionListener(), this);
     }
 
